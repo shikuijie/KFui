3238f7324584e2f7f05827d4ab75354828132a67
simon <skj9798@163.com>
2016-04-05 20:38:52 +0800

    初步添加了一个日期模块

--- src/index.tpl
+++ src/index.tpl
@@ -29,22 +29,7 @@
   <br>
 
   <kf-tree class="tree" :tree="treeData" :draggable="true"></kf-tree>
-
-
-  <kf-circle-slide class="slide" axis="x" :current="curSlideItem" @click="curSlideItem--">
-    <div>0</div>
-    <div>1</div>
-    <div>2</div>
-    <div>3</div>
-    <div>4</div>
-    <div>5</div>
-    <div>6</div>
-    <div>7</div>
-    <div>8</div>
-    <div>9</div>
-    <div>10</div>
-    <div>11</div>
-  </kf-circle-slide>
+  <kf-date></kf-date>
 
   <script>
     System.import('./index.js');
--- src/module/date/date.js
+++ src/module/date/date.js
@@ -1,4 +1,5 @@
 import vue from 'vue';
+import '../slide/slide';
 import './date.css!';
 import cls from './date.css.map';
 
@@ -7,20 +8,21 @@ vue.component('kf-date', {
     return {
       cls: cls,
       active: 1,
-      range: _.range(1, 30)
+      months: ['1月', '2月', '3月', '4月', '5月', '6月', '7月', '8月', '9月', '10月', '11月', '12月'],
+      curMonth: 0
     };
   },
   methods: {
-    getActiveCls: function(index) {
-      return this.active == index ? cls.active : '';
-    },
-    mouseDown: function(event) {
-      event.stopPropagation();
-      let wrapperHeight = this.$el.getBoundingClientRect(),
-          contentHeight = this.$el.querySelector('ul');
-    }
   },
   template:
-    '<div>' +
+    '<div :class="cls.date">' +
+      '<div :class="cls.monthWrapper">' +
+        '<div :class="cls.left" @click="curMonth++"></div>' +
+        '<div :class="cls.right" @click="curMonth--"></div>' +
+
+        '<kf-circle-slide :class="cls.month" axis="y" :current="curMonth">' +
+          '<span v-for="m in months" v-text="m"></span>' +
+        '</kf-circle-slide>' +
+      '</div>' +
     '</div>'
 });
