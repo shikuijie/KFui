ad43234790ec7dcc393992d476b438a5d3e615ed
simon <skj9798@163.com>
2016-05-13 17:19:00 +0800

    安装vue-router和vue-resource

--- src/module/lib.js
+++ src/module/lib.js
@@ -1,5 +1,7 @@
 import 'font-awesome';
 import vue from 'vue';
+import vueRouter from 'vue-router';
+import vueResource from 'vue-resource';
 import _ from 'lodash';
 import './form/form';
 import kfModal from './modal/modal';
@@ -10,4 +12,4 @@ import kfTable from './table/table';
 import kfTree from './tree/tree';
 import kfToaster from './toaster/toaster';
 
-export {_, vue, kfModal, kfTable, kfTree, kfToaster};
+export {_, vue, vueRouter, vueResource, kfModal, kfTable, kfTree, kfToaster};
