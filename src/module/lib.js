1b0d2008f5b147eff7cdf21083bfbe58fed68adc
simon <skj9798@163.com>
2016-06-02 19:23:24 +0800

    修改date模块

--- src/module/lib.js
+++ src/module/lib.js
@@ -7,7 +7,7 @@ import kfModal from './modal/modal';
 import './pager/pager';
 import './style/style';
 import './tab/tab';
-import './loading/loading'
+import './loading/loading';
 import kfTable from './table/table';
 import kfTree from './tree/tree';
 import kfToaster from './toaster/toaster';
